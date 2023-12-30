eb060af16f11b0c9f19f22b4da99162a68d440bf
jawney-anshita <jawney.anshita@gmail.com>
2023-12-30 13:22:20 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
+    output: 'export',
 
     // experimental: {
     //     appDir: true
     //   },
     // exclude:[".git", ".github"],
-      eslint: {
-        ignoreDuringBuilds: true,
-      },
-      typescript: {
-        ignoreBuildErrors: true,
-      },
-      reactStrictMode: true,
-      i18n: {
-        locales: ["en"],
-        defaultLocale: "en",
-      },
+    //   eslint: {
+    //     ignoreDuringBuilds: true,
+    //   },
+    //   typescript: {
+    //     ignoreBuildErrors: true,
+    //   },
+    //   reactStrictMode: true,
+    //   i18n: {
+    //     locales: ["en"],
+    //     defaultLocale: "en",
+    //   },
 
 
    
